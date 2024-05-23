7639707d3754159ccc57684d7b0c198ac63ced3a
changbenlong <changbenlong@gmail.com>
2024-05-23 22:52:52 +0800

    使用concatMap操作符进行组合请求

--- app/src/main/java/com/example/kotlinmvp/main/presenter/LoginPresenter.kt
+++ app/src/main/java/com/example/kotlinmvp/main/presenter/LoginPresenter.kt
@@ -1,5 +1,6 @@
 package com.example.kotlinmvp.main.presenter
 
+import com.example.baselib.bean.UserInfoBean
 import com.example.baselib.bean.WXArticleBean
 import com.example.baselib.https.ApiResponse
 import com.example.kotlinmvp.main.contract.LoginContract
@@ -56,6 +57,30 @@ class LoginPresenter : LoginContract.Presenter() {
             }
 
         })
+
+        //提醒，如果第二个接口请求参数需要从第一个接口返回的数据里去，可以是 concatMap 进行组合请求
+//        wrapObservable(mModel.getTempAccessToken().concatMap { autnBen ->
+//            return@concatMap mModel.login(autnBen.accessToken)
+//        }).subscribe(object : Observer<ApiResponse<UserInfoBean?>>{
+//                override fun onSubscribe(d: Disposable) {
+//                    listDisposable.add(d)
+//                }
+//
+//                override fun onError(e: Throwable) {
+//                }
+//
+//                override fun onComplete() {
+//                }
+//
+//                override fun onNext(response: ApiResponse<UserInfoBean?>) {
+//                    if (response.data == null) {
+//                        return
+//                    }
+//                    getView().lgoinSuccess(response.data!!)
+//                }
+//
+//            }
+//        })
 
     }
 }
